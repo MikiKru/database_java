a06b6669f539ba2d60c37fccd517406c397c6bd2
MikiKru <michal_kruczkowski@o2.pl>
2020-02-11 20:15:05 +0100

    JDBC start

--- database_connector/src/main/java/DatabaseConnection.java
+++ database_connector/src/main/java/DatabaseConnection.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 // JDBC     -> java database connector
 // mysql    -> silnik interpretera
@@ -8,8 +6,9 @@ public class DatabaseConnection {
     private String url = "jdbc:mysql://remotemysql.com:3306/lnuCMNEYTL";
     private String username = "lnuCMNEYTL";
     private String password = "vfJaJpLReH";
+    private Connection connection;
 
-    public Connection connectToDatabase() throws SQLException {
+    public void connectToDatabase() throws SQLException {
         // metoda statyczna z klasy DriverMananger pochodzącej z java-connector
         // -> zwraca obiekt połączenia klasy Connection
         Connection connection = DriverManager.getConnection(url, username, password);
@@ -18,11 +17,28 @@ public class DatabaseConnection {
         } else {
             System.out.println("... nie połączono z bazą danych ...");
         }
-        return connection;
+        this.connection = connection;
+    }
+    // metoda pobierająca i wypisująca na konsoli wszystkich praowników
+    public void getAllEmployees() throws SQLException {
+        String sql = "SELECT * FROM employee";
+        // przygotowanie zapytania SQL
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        // wykonanie zapytania -> zwracającego rezultat i nie wprowadzającego zmian w DB
+        ResultSet resultSet = preparedStatement.executeQuery();
+        // ResultSet to tablica 2D
+        while (resultSet.next()){
+            System.out.println("Pracownik nr: " + resultSet.getInt(1));
+            System.out.println("Imię: " + resultSet.getString(2));
+            System.out.println("Nazwisko: " + resultSet.getString(3));
+            System.out.println("Pensja netto: " + resultSet.getDouble(4));
+            System.out.println("==================================================");
+        }
     }
 
     public static void main(String[] args) throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         databaseConnection.connectToDatabase();
+        databaseConnection.getAllEmployees();
     }
 }
