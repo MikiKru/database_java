8ca3274ceebcf371410bcd461af007f02bc917c3
MikiKru <michal_kruczkowski@o2.pl>
2020-02-11 19:50:49 +0100

    database connection

---
+++ database_connector/src/main/java/DatabaseConnection.java
@@ -0,0 +1,5 @@
+public class DatabaseConnection {
+    private String url = "jdbc:mysql://remotemysql.com:3306/lnuCMNEYTL";
+    private String username = "lnuCMNEYTL";
+    private String password = "vfJaJpLReH";
+}
